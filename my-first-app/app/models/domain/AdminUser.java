63505bb647d8adc1836722380c0ef514959830a1
catherine <catherine.lyn@daumkakao.com>
2015-11-27 18:18:05 +0900

    add date

--- my-first-app/app/models/domain/AdminUser.java
+++ my-first-app/app/models/domain/AdminUser.java
@@ -3,6 +3,7 @@ package models.domain;
 
 import javax.persistence.*;
 import java.sql.Date;
+import java.util.Calendar;
 import java.util.List;
 
 import com.avaje.ebean.Model;
@@ -18,8 +19,8 @@ public class AdminUser extends Model{
    @Id
    public String id;
 
-   //@Formats.DateTime(pattern = "yyyy-mm-dd HH:MM:DD")
-   //public Date lastVisit;
+   @Formats.DateTime(pattern = "yyyy-mm-dd HH:MM:DD")
+   public Date lastVisit = new java.sql.Date(Calendar.getInstance().getTime().getTime());
    public String role="N";
 
 }
